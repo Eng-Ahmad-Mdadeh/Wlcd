3d8a7fe97e6450132ae80af8aa876ab05a76b11e
Ahmad Mdadeh <eng.ahmadmdadeh@gmail.com>
2026-06-22 10:27:19 +0300

    Tmp

--- lib/presentation/screens/terms_and_conditions/terms_and_conditions_screen.dart
+++ lib/presentation/screens/terms_and_conditions/terms_and_conditions_screen.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wlcd/core/resources/app_colors.dart';
-import 'package:wlcd/core/resources/app_fonts.dart';
-import 'package:wlcd/core/resources/app_values.dart';
-import 'package:wlcd/presentation/screens/notifications/widgets/notifications_home_indicator.dart';
-import 'package:wlcd/presentation/screens/terms_and_conditions/widgets/terms_and_conditions_dummy_data.dart';
 import 'package:wlcd/presentation/widgets/custom_app_bar.dart';
 import 'package:wlcd/presentation/widgets/text/body_title.dart';
-import 'package:wlcd/presentation/widgets/text/section_title.dart';
 
 class TermsAndConditionsScreen extends StatelessWidget {
   const TermsAndConditionsScreen({super.key});
@@ -16,55 +11,7 @@ class TermsAndConditionsScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.backGround,
       appBar: const CustomAppBar(title: 'الشروط والأحكام', showBackButton: true),
-      body: Center(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: AppWidth.w428),
-          child: Column(
-            children: [
-              Expanded(
-                child: SingleChildScrollView(
-                  padding: EdgeInsets.fromLTRB(
-                    AppPaddingWidth.p17,
-                    AppPaddingHeight.p25,
-                    AppPaddingWidth.p17,
-                    AppPaddingHeight.p20,
-                  ),
-                  child: Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.all(AppPaddingWidth.p20),
-                    decoration: BoxDecoration(
-                      color: AppColors.white,
-                      borderRadius: BorderRadius.circular(AppRadius.r13),
-                      border: Border.all(color: AppColors.notificationBorder, width: AppWidth.w1),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SectionTitle(text: 'نصوص مؤقتة', maxLines: 2),
-                        SizedBox(height: AppHeight.h16),
-                        for (int index = 0; index < termsAndConditionsDummySections.length; index++) ...[
-                          SectionTitle(text: 'بند تجريبي ${index + 1}', fontSize: AppFontSize.s15, maxLines: 2),
-                          SizedBox(height: AppHeight.h8),
-                          BodyTitle(
-                            text: termsAndConditionsDummySections[index],
-                            color: AppColors.notificationBodyText,
-                            fontSize: AppFontSize.s14,
-                            fontWeight: AppFontWeight.regular,
-                            height: 1.7,
-                            overflow: TextOverflow.visible,
-                          ),
-                          SizedBox(height: AppHeight.h16),
-                        ],
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const NotificationsHomeIndicator(),
-            ],
-          ),
-        ),
-      ),
+      body: Center(child: BodyTitle(text: "الشروط والأحكام")),
     );
   }
 }
