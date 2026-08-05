efcde8bd260deee222429bd18c287923a51f775a
Ahmad Mdadeh <eng.ahmadmdadeh@gmail.com>
2026-08-05 12:40:32 +0300

    Tmp

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -4,4 +4,11 @@ class ApiEndpoints {
   //#region Base Url
   static const String baseUrl = 'https://dev-endpoint/api/user';
   //#endregion
+
+
+  //#region Auth
+  static const String auth = '/auth';
+  static const String login = '/login';
+  static const String checkCode = '/check-code';
+//#endregion
 }
---
+++ lib/data/data_sources/auth/auth_remote_data_source.dart
@@ -0,0 +1,41 @@
+import 'package:wlcd/domain/entity/auth/auth_entity.dart';
+import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+import 'package:wlcd/core/constants/api_endpoints.dart';
+import 'package:wlcd/core/exceptions/app_exception.dart';
+import 'package:wlcd/data/data_sources/base/base_remote_data_source.dart';
+import 'package:wlcd/data/model/auth/auth_model.dart';
+import 'package:wlcd/data/model/base/base_model.dart';
+
+@Injectable()
+class AuthRemoteDataSource extends BaseRemoteDataSource<AuthModel> {
+  AuthRemoteDataSource() : super(ApiEndpoints.auth);
+
+  Future<Either<AppException, BaseModel<AuthModel>?>> login(AuthEntity data) {
+    return postData(
+      endpoint: ApiEndpoints.login,
+      fromJsonT: (json) => AuthModel.fromJson(json as Map<String, dynamic>),
+      data: data.toJson(),
+    );
+  }
+
+  Future<Either<AppException, BaseModel<AuthModel>?>> checkCode(AuthEntity data) {
+    return postData(
+      endpoint: ApiEndpoints.checkCode,
+      fromJsonT: (json) => AuthModel.fromJson(json as Map<String, dynamic>),
+      data: data.toJson(),
+    );
+  }
+
+  Future<Either<AppException, BaseModel<AuthModel>?>> resendCode(AuthEntity data) {
+    return postData(
+      endpoint: ApiEndpoints.login,
+      // fromJsonT: (json) => AuthModel.fromJson(json as Map<String, dynamic>),
+      data: data.toJson(),
+    );
+  }
+
+  // Future<Either<AppException, AuthModel>> logout() {
+  //   throw UnimplementedError('This function is not exist in remote use local instead');
+  // }
+}
