12a76be5f538d05543d1b11995f8ae3828508a05
Ahmad Mdadeh <eng.ahmadmdadeh@gmail.com>
2026-08-09 00:39:40 +0300

    Tmp

---
+++ lib/domain/repository/auth/i_auth_repository.dart
@@ -0,0 +1,15 @@
+// import 'package:tamalok/domain/entity/auth/auth_entity.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:tamalok/core/exceptions/app_exception.dart';
+// import 'package:tamalok/data/model/auth/auth_model.dart';
+// import 'package:tamalok/data/model/base/base_model.dart';
+//
+// abstract interface class IAuthRepository {
+//   Future<Either<AppException, BaseModel<AuthModel>?>> login(AuthEntity data);
+//
+//   Future<Either<AppException, BaseModel<AuthModel>?>> checkCode(AuthEntity data);
+//
+//   Future<Either<AppException, BaseModel<AuthModel>?>> resendCode(AuthEntity data);
+//
+//   Future<Either<AppException, void>> logout();
+// }
---
+++ lib/domain/usecase/auth/logout_usecase.dart
@@ -0,0 +1,18 @@
+// import 'package:tamalok/domain/repository/auth/i_auth_repository.dart';
+// import 'package:tamalok/domain/usecase/i_use_case.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:injectable/injectable.dart';
+// import '../../../core/exceptions/app_exception.dart';
+//
+// @Injectable(as: IUseCase<void, Null>)
+// @Named('LogOut')
+// class LogoutUsecase implements IUseCase<void, Null> {
+//   final IAuthRepository _repository;
+//
+//   LogoutUsecase(this._repository);
+//
+//   @override
+//   Future<Either<AppException, void>> call(Null n) {
+//     return _repository.logout();
+//   }
+// }
