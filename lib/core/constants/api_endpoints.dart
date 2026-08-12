1c136b008316ce6b45e5fb4767b70764ce395a78
Ahmad Mdadeh <eng.ahmadmdadeh@gmail.com>
2026-08-12 17:13:22 +0300

    Restore CheckCode authentication flow

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -20,5 +20,7 @@ class ApiEndpoints {
   static const String resetPassword = '/password/reset';
   static const String logout = '/logout';
   static const String session = '/session';
+  static const String login = '/login';
+  static const String checkCode = '/check-code';
 //#endregion
 }
--- lib/data/data_sources/auth/auth_remote_data_source.dart
+++ lib/data/data_sources/auth/auth_remote_data_source.dart
@@ -1,3 +1,4 @@
+import 'package:wlcd/domain/entity/auth/auth_entity.dart';
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 import 'package:wlcd/core/constants/api_endpoints.dart';
@@ -167,6 +168,29 @@ class AuthRemoteDataSource extends BaseRemoteDataSource<AuthModel> {
         wrappedResponse: false,
       );
 
+  Future<Either<AppException, BaseModel<AuthModel>?>> login(AuthEntity data) {
+    return postData(
+      endpoint: ApiEndpoints.login,
+      fromJsonT: (json) => AuthModel.fromJson(json as Map<String, dynamic>),
+      data: data.toJson(),
+    );
+  }
+
+  Future<Either<AppException, BaseModel<AuthModel>?>> checkCode(AuthEntity data) {
+    return postData(
+      endpoint: ApiEndpoints.checkCode,
+      fromJsonT: (json) => AuthModel.fromJson(json as Map<String, dynamic>),
+      data: data.toJson(),
+    );
+  }
+
+  Future<Either<AppException, BaseModel<AuthModel>?>> resendCode(AuthEntity data) {
+    return postData(
+      endpoint: ApiEndpoints.login,
+      // fromJsonT: (json) => AuthModel.fromJson(json as Map<String, dynamic>),
+      data: data.toJson(),
+    );
+  }
 
   // Future<Either<AppException, AuthModel>> logout() {
   //   throw UnimplementedError('This function is not exist in remote use local instead');
