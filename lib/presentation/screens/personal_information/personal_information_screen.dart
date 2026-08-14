2291b66d681c004b7994dc26897200da8094c386
Ahmad Mdadeh <eng.ahmadmdadeh@gmail.com>
2026-08-14 18:31:59 +0300

    feat: load personal information from profile bloc

--- lib/presentation/screens/personal_information/personal_information_screen.dart
+++ lib/presentation/screens/personal_information/personal_information_screen.dart
@@ -1,10 +1,13 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:wlcd/core/resources/app_assets.dart';
 import 'package:wlcd/core/resources/app_values.dart';
 import 'package:wlcd/core/routes/app_routes_imports.dart';
-
+import 'package:wlcd/data/model/profile/profile_model.dart';
+import 'package:wlcd/presentation/bloc/profile/get_profile/get_profile_bloc.dart';
 import 'package:wlcd/presentation/widgets/custom_app_bar.dart';
-import 'package:wlcd/presentation/widgets/custom_drop_down_widget.dart';
+import 'package:wlcd/presentation/widgets/loading_widget.dart';
+import 'package:wlcd/presentation/widgets/retry_widget.dart';
 
 import 'widgets/info_text_field.dart';
 import 'widgets/picture_section.dart';
@@ -15,7 +18,10 @@ class PersonalInformationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BodyPersonalInformationScreen();
+    return BlocProvider(
+      create: (_) => GetProfileBloc()..add(const SubmitGetProfileEvent()),
+      child: const BodyPersonalInformationScreen(),
+    );
   }
 }
 
@@ -25,67 +31,89 @@ class BodyPersonalInformationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title: "المعلومات الشخصية", showBackButton: true),
-      body: Stack(
-        fit: StackFit.expand,
-        alignment: Alignment.bottomCenter,
-        children: [
-          SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsetsDirectional.only(
-                bottom: AppPaddingHeight.p100,
-                start: AppPaddingWidth.p20,
-                end: AppPaddingWidth.p20,
-              ),
-              child: Column(
-                spacing: AppHeight.h20,
-                children: [
-                  PictureSection(image: AppAssets.logo),
-                  SizedBox(height: AppHeight.h1),
-                  InfoTextField(
-                    label: "الإسم الكامل",
-                    icon: Iconsax.user_outline,
-                    initValue: "سامي المرعي",
-                    onChanged: (p0) {},
-                  ),
-                  InfoTextField(
-                    label: "البريد الإلكتروني",
-                    icon: Iconsax.sms_outline,
-                    initValue: "wlcd@info.sy",
-                    onChanged: (p0) {},
-                  ),
-                  InfoTextField(
-                    label: "رقم الهاتف",
-                    icon: Iconsax.call_outline,
-                    initValue: "0938544444",
-                    onChanged: (p0) {},
-                  ),
-                  // CustomDropDownWidget(
-                  //   startPadding: AppPaddingWidth.p20,
-                  //   onChanged: (value) {},
-                  //   hintText: "السنة الدراسية",
-                  //   items: ["ثانوي", "إعدادي", "إبتدائي"],
-                  //   initialItem: "إعدادي",
-                  //   borderRadius: AppRadius.r13,
-                  //   isStringList: true,
-                  // ),
-
-                ],
-              ),
-            ),
-          ),
-          PositionedDirectional(
-            start: 0,
-            end: 0,
-            bottom: 0,
-            child: SaveButtonSection(
-              onPressed: () {
-                FocusManager.instance.primaryFocus?.unfocus();
-              },
-            ),
-          ),
-        ],
+      appBar: const CustomAppBar(
+        title: "المعلومات الشخصية",
+        showBackButton: true,
       ),
+      body: BlocBuilder<GetProfileBloc, IGetProfileState>(
+        builder: (context, state) {
+          if (state is GetProfileLoaded && state.profileModel?.data != null) {
+            return _PersonalInformationContent(
+              profile: state.profileModel!.data!,
+            );
+          }
+
+          if (state is GetProfileFailed || state is GetProfileLoaded) {
+            return RetryWidget(
+              onReload: () => context.read<GetProfileBloc>().add(
+                const SubmitGetProfileEvent(),
+              ),
+            );
+          }
+
+          return const LoadingWidget(0);
+        },
+      ),
+    );
+  }
+}
+
+class _PersonalInformationContent extends StatelessWidget {
+  const _PersonalInformationContent({required this.profile});
+
+  final ProfileModel profile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      alignment: Alignment.bottomCenter,
+      children: [
+        SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsetsDirectional.only(
+              bottom: AppPaddingHeight.p100,
+              start: AppPaddingWidth.p20,
+              end: AppPaddingWidth.p20,
+            ),
+            child: Column(
+              spacing: AppHeight.h20,
+              children: [
+                PictureSection(image: profile.avatar?.url ?? AppAssets.logo),
+                SizedBox(height: AppHeight.h1),
+                InfoTextField(
+                  label: "الإسم الكامل",
+                  icon: Iconsax.user_outline,
+                  initValue: profile.displayName,
+                  onChanged: (p0) {},
+                ),
+                InfoTextField(
+                  label: "البريد الإلكتروني",
+                  icon: Iconsax.sms_outline,
+                  initValue: profile.email ?? '',
+                  onChanged: (p0) {},
+                ),
+                InfoTextField(
+                  label: "رقم الهاتف",
+                  icon: Iconsax.call_outline,
+                  initValue: profile.phone ?? '',
+                  onChanged: (p0) {},
+                ),
+              ],
+            ),
+          ),
+        ),
+        PositionedDirectional(
+          start: 0,
+          end: 0,
+          bottom: 0,
+          child: SaveButtonSection(
+            onPressed: () {
+              FocusManager.instance.primaryFocus?.unfocus();
+            },
+          ),
+        ),
+      ],
     );
   }
 }
